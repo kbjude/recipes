9ffc302cffa893289a51d97e6fdb60517daa7634
Jude Kajura <kbjude@gmail.com>
2021-03-14 05:14:37 +0300

    Work on the reducer

--- recipes/src/index.js
+++ recipes/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import Reducer from './store/reducer';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
 

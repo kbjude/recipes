e48e7c8215deb3519e0bb6aefc12ec26903ca05c
Jude Kajura <kbjude@gmail.com>
2021-03-27 00:24:44 +0300

    Adding routes

---
+++ recipes/src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+const routes = () => {
+    <div>
+        <BrowserRouter>
+            <Switch>
+
+            </Switch>
+        </BrowserRouter>
+    </div>
+}
